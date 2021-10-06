8945851e197a8e45ff46afe577995bd00ab6a197
OWEEGOOSS <91886082+OWEEGOOSS@users.noreply.github.com>
2021-10-06 15:03:14 +0200

    Add files via upload

---
+++ WorkLifeBalance.py
@@ -0,0 +1,14 @@
+import webbrowser
+import time
+
+repeats = 0
+
+while repeats < 5:
+	time.sleep(5400)
+	print("Zeit für eine Pause!")
+	webbrowser.open("https://www.youtube.com/watch?v=eXLSBdxm_cs")
+	time.sleep(400)
+	print("Zeit zum Weiterarbeiten!\n")
+	repeats += 1
+
+print("Ende!")
